368efb73ea06b9a20b19ea05638102679139b547
Ignacio Chiappe <nacho.chiappe@gmail.com>
2015-09-23 11:52:08 -0300

    Se agrega archivo inicial de pruebas (vacio) para permitir compilación.

---
+++ pruebas_alumno.c
@@ -0,0 +1,12 @@
+#include "lista.h"
+#include "testing.h"
+#include <stddef.h>
+#include <stdio.h>
+
+/* ******************************************************************
+ *                   PRUEBAS UNITARIAS ALUMNO
+ * *****************************************************************/
+
+void pruebas_lista_alumno() {
+	
+}
